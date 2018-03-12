3c6c22be7424e0a72bbc4755b660c65e88e98431
Megavolt <dserov@gmail.com>
2018-03-13 00:29:27 +0300

    добавлены иконки, добавлены подсказки, добавлено  контексное меню

--- Lesson4/MainDz/src/main/java/ChatController.java
+++ Lesson4/MainDz/src/main/java/ChatController.java
@@ -1,2 +1,72 @@
+import de.jensd.fx.fontawesome.AwesomeIcon;
+import de.jensd.fx.fontawesome.Icon;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.util.Callback;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+
 public class ChatController {
+    @FXML
+    TextField msgField;
+
+    @FXML
+    TextArea textChat;
+
+    @FXML
+    ListView<String> usersList;
+
+    /**
+     * Отображение даты и временив окне чата
+     *
+     */
+    @FXML
+    public void sendMessage(ActionEvent event) {
+        String msg = msgField.getText();
+        if (msg.length() == 0) return;
+
+        LocalDateTime localDateTime = LocalDateTime.now();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MM-yy HH:mm:ss");
+        String dt = localDateTime.format(dateTimeFormatter);
+        textChat.appendText(dt + ": " + msg + "\n");
+        msgField.clear();
+    }
+
+    @FXML
+    public void initialize() {
+        ObservableList<String> names = FXCollections.observableArrayList(
+                "Маша", "Наташа", "Иван", "Настя", "Вася", "Света", "Андрей");
+        usersList.setItems(names);
+
+        usersList.setCellFactory(new Callback<ListView<String>, ListCell<String>>() {
+            @Override
+            public ListCell<String> call(ListView<String> list) {
+                return new UserCell();
+            }
+        });
+    }
+
+    static class UserCell extends ListCell<String> {
+        final Icon icon = new Icon(AwesomeIcon.USER, "1em", "", "");
+
+        @Override
+        public void updateItem(String item, boolean empty) {
+            super.updateItem(item, empty);
+            if (!empty) {
+                setGraphic(icon);
+                setText(item);
+            } else {
+                setGraphic(null);
+                setText("");
+            }
+        }
+    }
 }
--- Lesson4/MainDz/src/main/java/EntryPoint.java
+++ Lesson4/MainDz/src/main/java/EntryPoint.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ListView;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -19,14 +18,8 @@ public class EntryPoint extends Application {
     public void start(Stage primaryStage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("chat.fxml"));
         Scene scene = new Scene(root, 600, 300);
-//        scene.getStylesheets().add((getClass().getResource("/css/style.css").toExternalForm()));
         primaryStage.setScene(scene);
         primaryStage.setTitle("My Chat");
         primaryStage.show();
-
-        ListView<String> users_list = (ListView) scene.lookup("#users_list");
-        ObservableList<String> names = FXCollections.observableArrayList(
-         "Julia", "Ian", "Sue", "Matthew", "Hannah", "Stephan", "Denise");
-        users_list.setItems(names);
     }
 }
